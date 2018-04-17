153bd849accaf820346c8e3649170c6ce85abc58
B Money <36802937+bristolmc@users.noreply.github.com>
2018-04-17 11:48:21 -0500

    Update MPI.c

--- MPI/MPI.c
+++ MPI/MPI.c
@@ -5,11 +5,20 @@
 
 int longestline()
 {
-	printf("hi");
-	FILE *f = fopen("~/dan/625/wiki_dump.txt", "r"); //open file
-	printf("file opened");
+	printf("finding the longest line length \n");
+	FILE *f = fopen("~/dan/625/wiki_dump.txt", "rb"); //open file
+	
+	if (f == NULL)
+	{
+		printf("failed to open \n");
+		return -1;
+	}
+	
+	printf("file opened \n");
 	fseek(f, 0, SEEK_END); //run to the end of the file to find its length
 	long size = ftell(f); //size is the total number of characters in the file
+	printf("%d", size);
+	printf("\n");
 	const char * wiki = malloc(size +1); //allocating the string
 	fread(wiki, size, 1, f); //reading the file into wiki string
 	printf("file read");
