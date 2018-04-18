22e19777c080945fd4901650f3126656d9b5f0ce
coreyvessar <coreyvessar@ksu.edu>
2018-04-18 11:52:27 -0500

    Update proj4.c

--- basecode/proj4.c
+++ basecode/proj4.c
@@ -65,42 +65,68 @@ void printResults()
   }
 }
 
-char * longestCommonSubstring(const char * str1, const char * str2)
+char* longestCommonSubstring(char* X, char* Y)
 {
-
-  int *curr = new int [str2.size()];
-  int *prev = new int [str2.size()];
-  int *swap = NULL;
-  int maxSubstr = 0;
-  char * longest;
-  for(unsigned int i = 0; i < str1.size(); ++i)
-  {
-    for(unsigned int j = 0; j < str2.size(); ++j)
-    {
-      if(str1[i] != str2[j])
-            curr[j] = 0;
-      else
-      {
-        if(i == 0 || j == 0)
-            curr[j] = 1;
-        else
-            curr[j] = 1 + prev[j-1];
-        if(maxSubstr < curr[j])
-        {
-            maxSubstr = curr[j];
-            longest.clear();
+    // Create a table to store lengths of longest common
+    // suffixes of substrings.   Note that LCSuff[i][j]
+    // contains length of longest common suffix of X[0..i-1]
+    // and Y[0..j-1]. The first row and first column entries
+    // have no logical meaning, they are used only for
+    // simplicity of program
+    int m = strlen(X);
+    int n = strlen(Y);
+    int LCSuff[m + 1][n + 1];
+ 
+    // To store length of the longest common substring
+    int len = 0;
+ 
+    // To store the index of the cell which contains the 
+    // maximum value. This cell's index helps in building 
+    // up the longest common substring from right to left.
+    int row, col;
+ 
+    /* Following steps build LCSuff[m+1][n+1] in bottom
+       up fashion. */
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0 || j == 0)
+                LCSuff[i][j] = 0;
+ 
+            else if (X[i - 1] == Y[j - 1]) {
+                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
+                if (len < LCSuff[i][j]) {
+                    len = LCSuff[i][j];
+                    row = i;
+                    col = j;
+                }
+            } else
+                LCSuff[i][j] = 0;
         }
-        if (maxSubstr == curr[j])
-            longest += str1.substr(i - maxSubstr + 1, i + 1);
-      }
     }
-    swap=curr;
-    curr=prev;
-    prev=swap;
-  }
-  delete [] curr;
-  delete [] prev;
-  return (longest.substr(0, maxSubstr));
+ 
+    // if true, then no common substring exists
+    if (len == 0) {
+        char* t = "No Common Substring";
+        return t ;
+    }
+ 
+    // allocate space for the longest common substring
+    char* resultStr = (char*)malloc((len + 1) * sizeof(char));
+    resultStr[len] = '&#092;&#048;';
+ 
+    // traverse up diagonally form the (row, col) cell
+    // until LCSuff[row][col] != 0
+    while (LCSuff[row][col] != 0) {
+        resultStr[--len] = X[row - 1]; // or Y[col-1]
+ 
+        // move diagonally up to previous cell
+        row--;
+        col--;
+    }
+ 
+    // required longest common substring
+    return resultStr;
+    
 }
     
 
