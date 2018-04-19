c0b5b4479cbfed8d9f14cef3027e708159626a9e
coreyvessar <coreyvessar@ksu.edu>
2018-04-19 13:02:26 -0500

    Update proj4.c

--- basecode/proj4.c
+++ basecode/proj4.c
@@ -68,64 +68,24 @@ void printResults()
 
 char* longestCommonSubstring(char* X, char* Y)
 {
-    // Create a table to store lengths of longest common
-    // suffixes of substrings.   Note that LCSuff[i][j]
-    // contains length of longest common suffix of X[0..i-1]
-    // and Y[0..j-1]. The first row and first column entries
-    // have no logical meaning, they are used only for
-    // simplicity of program
-    int m = strlen(X);
-    int n = strlen(Y);
-    int LCSuff[m + 1][n + 1];
- 
-    // To store length of the longest common substring
-    int len = 0;
- 
-    // To store the index of the cell which contains the 
-    // maximum value. This cell's index helps in building 
-    // up the longest common substring from right to left.
-    int row, col;
- 
-    /* Following steps build LCSuff[m+1][n+1] in bottom
-       up fashion. */
-    int i,j;
-    for ( i = 0; i <= m; i++) {
-        for ( j = 0; j <= n; j++) {
-            if (i == 0 || j == 0)
-                LCSuff[i][j] = 0;
- 
-            else if (X[i - 1] == Y[j - 1]) {
-                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
-                if (len < LCSuff[i][j]) {
-                    len = LCSuff[i][j];
-                    row = i;
-                    col = j;
-                }
-            } else
-                LCSuff[i][j] = 0;
-        }
+    int lenA = 0;
+    int lenB = 0;
+    char* looper = X;
+    while(looper != '/n')
+    {
+        lenA++;
+        looper++;
     }
- 
-    // if true, then no common substring exists
-    if (len == 0) {
-        char* t = "No Common Substring";
-        return t ;
+    looper = Y;
+    while(looper != '/n')
+    {
+        lenB++;
+        looper++;
     }
- 
-    // allocate space for the longest common substring
-    char* resultStr = (char*)malloc((len + 1) * sizeof(char));
-    resultStr[len] = '&#092;&#048;';
- 
-    // traverse up diagonally form the (row, col) cell
-    // until LCSuff[row][col] != 0
-    while (LCSuff[row][col] != 0) {
-        resultStr[--len] = X[row - 1]; // or Y[col-1]
- 
-        // move diagonally up to previous cell
-        row--;
-        col--;
-    }
- 
+    
+    
+    int j, i;
+    
     // required longest common substring
     return resultStr;
     
