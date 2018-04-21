5e70bf2c5bd0736357036431fb7d2d7c53f07ae9
coreyvessar <coreyvessar@ksu.edu>
2018-04-21 12:51:06 -0500

    Update basetrial.c

--- basecode/basetrial.c
+++ basecode/basetrial.c
@@ -77,22 +77,25 @@ void printResults()
 static void init(int s1_length, int s2_length){
     if (s1_length+1 > _matrix_row_size || s2_length+1 > _matrix_collumn_size){
 	/* free matrix */
-	for (int i = 0; i < _matrix_row_size; i++)
+	int i;
+	for (i = 0; i < _matrix_row_size; i++)
 	    free(_matrix[i]);
 	free(_matrix);
 	
 	/* malloc matrix */
 	_matrix = (int **)malloc((s1_length+1) * sizeof(int*));
-	for (int i = 0; i < s1_length+1; i++)
+	for (i = 0; i < s1_length+1; i++)
 	    _matrix[i] = (int *)malloc((s2_length+1) * sizeof(int));
 
 	_matrix_row_size = s1_length+1;
 	_matrix_collumn_size = s2_length+1;
     }
 
-    for (int i = 0; i <= s1_length; i++)
+    for (i = 0; i <= s1_length; i++)
 	_matrix[i][s2_length] = 0;
-    for (int j = 0; j <= s2_length; j++)
+	
+	int j;
+    for (j = 0; j <= s2_length; j++)
 	_matrix[s1_length][j] = 0;
 }
 
@@ -104,7 +107,8 @@ int LCS(char *s1, char *s2, char **longest_common_substring){
     init(s1_length, s2_length);
 
     int max_len = 0, max_index_i = -1;
-    for (int i = s1_length-1; i >= 0; i--){
+    int i;
+    for (i = s1_length-1; i >= 0; i--){
     	for (int j = s2_length-1; j >= 0; j--){
     	    if (s1[i] != s2[j]){
     		_matrix[i][j] = 0;
