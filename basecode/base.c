ddb36ffa8dad2ffe1e53e73449d543cf779f2b44
coreyvessar <coreyvessar@ksu.edu>
2018-04-24 10:19:24 -0500

    Update base.c

--- basecode/base.c
+++ basecode/base.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/time.h>
+//#include <sys/time.h>
 #include <stdbool.h>
 
 #define WIKI_ARRAY_SIZE 10
@@ -12,6 +12,11 @@
  char **longestSub;
 
  int lengthOfSubstring[WIKI_ARRAY_SIZE] ;
+
+typedef struct {
+  long tv_sec;
+  long tv_usec;
+} timeval;
  
 
 bool readToMemory();
