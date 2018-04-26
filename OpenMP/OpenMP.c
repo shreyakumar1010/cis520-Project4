27fe619749a83bdd6d78ab6c1d0a54e08b766121
coreyvessar <coreyvessar@ksu.edu>
2018-04-25 21:47:32 -0500

    Update OpenMP.c

--- OpenMP/OpenMP.c
+++ OpenMP/OpenMP.c
@@ -13,6 +13,7 @@ int LCS (char * s1, char * s2, char ** longest_common_substring);
 //load the lines into an array
 char  **wiki_array;
 char **longestSub;
+char **original = longestSub;
 
 int num_threads = 8;
 
@@ -146,11 +147,13 @@ void printToFile()
 void printResults()
 { 
   	int i;
-	longestSub = longestSub - (WIKI_ARRAY_SIZE - 1);
+	//longestSub = longestSub - (WIKI_ARRAY_SIZE - 1);
+	longestSub = original;
   	for(i = 0; i <= WIKI_ARRAY_SIZE - 2; i++)
   	{ 
-      		printf("%d-%d: %s", i , i + 1 ,longestSub[i]); 
+      		printf("%d-%d: %s", i , i + 1 ,&longestSub); 
       		printf("\n");
+		longestSub++;
   	}
 }
 
