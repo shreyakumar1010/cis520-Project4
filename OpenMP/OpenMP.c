bc8a38507042b1c1f3d165fb418d2c47abe7f8e9
coreyvessar <coreyvessar@ksu.edu>
2018-04-26 15:12:07 -0500

    Update OpenMP.c

--- OpenMP/OpenMP.c
+++ OpenMP/OpenMP.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include <omp.h>
 
-#define WIKI_ARRAY_SIZE 20
+#define WIKI_ARRAY_SIZE 500
 #define WIKI_LINE_SIZE 2001
 int lengthOfSubstring [WIKI_ARRAY_SIZE];
 int LCS (char * s1, char * s2, char ** longest_common_substring);
