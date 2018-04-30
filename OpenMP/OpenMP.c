c95da0f110774c666797a9ed28ba76aefe8165cd
coreyvessar <coreyvessar@ksu.edu>
2018-04-30 13:33:08 -0500

    Update OpenMP.c

--- OpenMP/OpenMP.c
+++ OpenMP/OpenMP.c
@@ -219,7 +219,7 @@ int LCS(char *s1, char *s2, char **longest_common_substring, int index)
     	{
 		omp_set_lock(&my_lock);
 		longest_common_substring[index] = malloc(sizeof(char) * (max_len+1));
-		if(*longest_common_substring == NULL) {
+		if(longest_common_substring[index] == NULL) {
 			perror("Could not allocate memory");
 			exit(0);
 		}
