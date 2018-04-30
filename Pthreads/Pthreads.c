c6f36fbce82ffccc2e70ab4498ad3473506e7860
coreyvessar <coreyvessar@ksu.edu>
2018-04-30 16:32:05 -0500

    Update Pthreads.c

--- Pthreads/Pthreads.c
+++ Pthreads/Pthreads.c
@@ -227,7 +227,7 @@ int LCS(char *s1, char *s2, char **longest_common_substring)
     	if (longest_common_substring != NULL)
     	{
 		pthread_mutex_lock(&lock)
-		*longest_common_substring = malloc(sizeof(char)*(max_len + 1));
+		*longest_common_substring = malloc((int)sizeof(char)*(int)(max_len + 1));
 		strncpy(*longest_common_substring, s1+max_index_i, max_len);
 		(*longest_common_substring)[max_len] = '\0';
 		pthread_mutex_unlock(&lock);
