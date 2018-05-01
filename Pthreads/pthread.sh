0531ca04b66783b77eed22a550e5797be0faa417
coreyvessar <coreyvessar@ksu.edu>
2018-05-01 09:21:19 -0500

    Update pthread.sh

--- Pthreads/pthread.sh
+++ Pthreads/pthread.sh
@@ -1,31 +1,6 @@
-##### These lines are for Slurm
-#SBATCH -N 2                      #Number of nodes to use
-#SBATCH -p pReserved              #Workshop queue
-#SBATCH -t 5:00                   #Maximum time required
-#SBATCH -o output.%j              #Output file name
+#!/bin/bash -l
 
-### Job commands start here 
-### Display some diagnostic information
-echo '=====================JOB DIAGNOTICS========================'
-date
-echo -n 'This machine is ';hostname
-echo -n 'My jobid is '; echo $SLURM_JOBID
-echo 'My path is:' 
-echo $PATH
-echo 'My job info:'
-squeue -j $SLURM_JOBID
-echo 'Machine info'
-sinfo -s
-
-echo '=====================JOB STARTING=========================='
-
-### CHANGE THE LINES BELOW TO SELECT DIFFERENT MPI CODES AND/OR COMPILERS
-#Compile an exercise code
-gcc -lpthread pthread.c
-#Run the code
-srun -N2 ./a.out
-
-### Issue the sleep so we have time to see the job actually running
-sleep 120
-echo ' ' 
-echo '========================ALL DONE==========================='
+for i 1 2 3 4 5 6 7 8 9 10
+do
+   homes/coreyvessar/cis520/cis520-Project4/Pthreads/Pthreads $1 $2
+done
