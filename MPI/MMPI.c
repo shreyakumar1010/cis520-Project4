2d899efec1a69a18d1da67f27e83962026443e76
coreyvessar <coreyvessar@ksu.edu>
2018-05-01 14:22:35 -0500

    Update MMPI.c

--- MPI/MMPI.c
+++ MPI/MMPI.c
@@ -7,7 +7,7 @@
 
 #define WIKI_ARRAY_SIZE 500
 #define WIKI_LINE_SIZE 2001
-#define num_threads 32
+//#define num_threads 32
 char * numCores;
 
 //int WIKI_ARRAY_SIZE;
@@ -21,7 +21,7 @@ char **longestSub;
 char** longestSubPointer;
 
 
-//int num_threads;
+int num_threads = 32;
 
 void readToMemory();
 void printResults();
